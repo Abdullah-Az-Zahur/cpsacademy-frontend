c882f44a920ee40a49b0482126cb979c034ec1de
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2025-10-01 23:49:03 +0600

    add : login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <section className="w-full h-lvh bg-white p-6 flex justify-center items-center">
+      <div className="max-w-2xl w-full flex flex-col items-center mx-auto rounded-3xl sm:p-6 p-4 bg-white border border-gray-200 shadow-xl">
+        <h1 className="flex justify-center font-semibold text-4xl text-primary mb-6"></h1>
+      {/* login from */}
+      </div>
+    </section>
+  );
 }
 
-export default Login
+export default Login;
