e06e44207a57bfa3df2fb83e8a6658a332e802a7
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2025-10-04 16:48:09 +0600

    add : image url

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["localhost", "img.youtube.com"], 
+    domains: [
+      "localhost",
+      "img.youtube.com",
+      "cpsacademy-backend-production-cefb.up.railway.app",
+    ],
   },
 };
 
