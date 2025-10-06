20418491c123000a15bf4839bd94facfa16800b4
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2025-10-06 18:51:20 +0600

    Normalize STRAPI_URL

--- src/lib/strapi.ts
+++ src/lib/strapi.ts
@@ -1,8 +1,9 @@
 import { Body, ContentBlock, NestedObject, UploadResult } from "@/types/strapi";
 import axios from "axios";
 
-const STRAPI_URL =
-  process.env.NEXT_PUBLIC_STRAPI_URL || "http://localhost:1337";
+const STRAPI_URL = (
+  process.env.NEXT_PUBLIC_STRAPI_URL || "http://localhost:1337"
+).replace(/\/$/, "");
 
 function parseStrapiError(data: unknown): string | null {
   try {
